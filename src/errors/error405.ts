84dd716c73daaa0fbb51beacd6d436987c691485
Tenebrosful <48710891+Tenebrosful@users.noreply.github.com>
2022-03-21 09:16:43 +0100

    Update error405.ts

--- src/errors/error405.ts
+++ src/errors/error405.ts
@@ -3,8 +3,10 @@ import { Request, Response } from "express";
 /**
  * Error 405 "Method Not Allowed"
  */
-export default function error405(req: Request, res: Response) {
-  res.status(405).json({
+export default function error405(req: Request, res: Response, allowedMethod: string[]) {
+  res
+    .set("Allow", allowedMethod)
+    .status(405).json({
     code: 405,
     message: "Méthode non-autorisée"
   });
